a1e1ed54892d93bb30a6bdd44b975d3a67faa934
mwauracyrus <159931996+mwauracyrus@users.noreply.github.com>
2024-03-12 13:27:57 +0300

    Create array.c

---
+++ array.c
@@ -0,0 +1,39 @@
+//MWAURA CYRUS NGIGI
+//ENE212-0060/2021
+
+#include <stdio.h>
+
+void multiplyMatrices(int mat1[3][3], int mat2[3][3], int result[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            result[i][j] = 0;
+            for (int k = 0; k < 3; k++) {
+                result[i][j] += mat1[i][k] * mat2[k][j];
+            }
+        }
+    }
+}
+
+void printMatrix(int mat[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int mat1[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int mat2[3][3] = {{9, 8, 7}, {6, 5, 4}, {3, 2, 1}};
+    int result[3][3];
+
+    // Multiply matrices
+    multiplyMatrices(mat1, mat2, result);
+
+    // Print result matrix
+    printf("Resultant Matrix:\n");
+    printMatrix(result);
+
+    return 0;
+}
